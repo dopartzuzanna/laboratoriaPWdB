2c525251ecf316270dc4f7e85250cb12a316c6c5
zuzanna <134936492+dopartzuzanna@users.noreply.github.com>
2026-04-15 17:00:33 +0200

    lab6
    raczej dziala :D

--- lab6/FormGame.Designer.cs
+++ lab6/FormGame.Designer.cs
@@ -28,19 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lblCzas = new System.Windows.Forms.Label();
             this.gridPlansza = new System.Windows.Forms.TableLayoutPanel();
+            this.timerGry = new System.Windows.Forms.Timer(this.components);
+            this.timerKrokodyl = new System.Windows.Forms.Timer(this.components);
+            this.timerSzop = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // lblCzas
             // 
             this.lblCzas.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.lblCzas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblCzas.Location = new System.Drawing.Point(0, 421);
+            this.lblCzas.Location = new System.Drawing.Point(0, 393);
             this.lblCzas.Name = "lblCzas";
-            this.lblCzas.Size = new System.Drawing.Size(431, 29);
+            this.lblCzas.Size = new System.Drawing.Size(431, 57);
             this.lblCzas.TabIndex = 0;
-            this.lblCzas.Text = "label1";
+            this.lblCzas.Text = "Czas";
             this.lblCzas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.lblCzas.Click += new System.EventHandler(this.lblCzas_Click);
             // 
@@ -56,8 +60,12 @@
             this.gridPlansza.RowCount = 2;
             this.gridPlansza.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.gridPlansza.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.gridPlansza.Size = new System.Drawing.Size(431, 421);
+            this.gridPlansza.Size = new System.Drawing.Size(431, 393);
             this.gridPlansza.TabIndex = 1;
+            // 
+            // timerGry
+            // 
+            this.timerGry.Tick += new System.EventHandler(this.timerGry_Tick);
             // 
             // FormGame
             // 
@@ -77,5 +85,8 @@
 
         private System.Windows.Forms.Label lblCzas;
         private System.Windows.Forms.TableLayoutPanel gridPlansza;
+        private System.Windows.Forms.Timer timerGry;
+        private System.Windows.Forms.Timer timerKrokodyl;
+        private System.Windows.Forms.Timer timerSzop;
     }
 }
--- lab6/FormGame.cs
+++ lab6/FormGame.cs
@@ -12,13 +12,20 @@ namespace lab6
 {
     public partial class FormGame : Form
     {
+        private int pozostalyCzas;
         public FormGame()
         {
             InitializeComponent();
+            pozostalyCzas = UstawieniaGry.Czas;
             GenerujPlansze();
-            lblCzas.Text = UstawieniaGry.Czas.ToString() + " s";
-        }
+            RozmiesczZwierzeta();
+            timerGry.Interval = 1000;
+            timerGry.Start();
+            timerKrokodyl.Interval = 2000;
+            timerKrokodyl.Tick += timerKrokodyl_Tick;
+            timerSzop.Tick += timerSzop_Tick;
 
+        }
         private void GenerujPlansze()
         {
             // Czyścimy siatkę TableLayoutPanel
@@ -50,13 +57,136 @@ namespace lab6
             }
         }
 
+
+        private void RozmiesczZwierzeta()
+        {
+            Random rnd = new Random();
+            var wszystkiePrzyciski = gridPlansza.Controls.OfType<Button>().ToList();
+
+            // Najpierw ustawiamy wszystkim "Puste"
+            foreach (var b in wszystkiePrzyciski) b.Tag = "Puste";
+
+            // Losujemy Dydelfy
+            RozdzielRole(wszystkiePrzyciski, "Dydelf", UstawieniaGry.LiczbaDydelfow, rnd);
+            // Losujemy Szopy 
+            RozdzielRole(wszystkiePrzyciski, "Szop", UstawieniaGry.LiczbaSzopow, rnd);
+            // Losujemy Krokodyle 
+            RozdzielRole(wszystkiePrzyciski, "Krokodyl", UstawieniaGry.LiczbaKrokodyli, rnd);
+        }
+
+        private void RozdzielRole(List<Button> przyciski, string rola, int ilosc, Random rnd)
+        {
+            int dodane = 0;
+            while (dodane < ilosc)
+            {
+                int index = rnd.Next(przyciski.Count);
+                if (przyciski[index].Tag.ToString() == "Puste")
+                {
+                    przyciski[index].Tag = rola;
+                    dodane++;
+                }
+            }
+        }
+
+        private int znalezioneDydelfy = 0;
+        private bool krokodylAktywny = false;
+        private Button ostatniSzop;
+
+        private void timerKrokodyl_Tick(object sender, EventArgs e)
+        {
+            timerKrokodyl.Stop();
+            if (krokodylAktywny)
+            {
+                timerGry.Stop();
+                MessageBox.Show("Porażka! Krokodyl Cię dopadł (brak reakcji w 2s)."); // 
+                this.Close();
+            }
+        }
+
+     
+
+        private void timerSzop_Tick(object sender, EventArgs e)
+        {
+            timerSzop.Stop();
+            if (ostatniSzop == null) return;
+
+            // Pobieramy współrzędne z nazwy przycisku (btn_x_y)
+            string[] parts = ostatniSzop.Name.Split('_');
+            int x = int.Parse(parts[1]);
+            int y = int.Parse(parts[2]);
+
+            // Pętla sprawdzająca sąsiadów (3x3 dookoła szopa)
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    int nx = x + i;
+                    int ny = y + j;
+
+                    // Szukamy przycisku o takich współrzędnych w gridzie
+                    Control[] found = gridPlansza.Controls.Find($"btn_{nx}_{ny}", true);
+                    if (found.Length > 0)
+                    {
+                        Button b = (Button)found[0];
+                        b.Enabled = true;
+                        b.BackColor = Color.Gray;
+                        b.Text = "";
+                    }
+                }
+            }
+        }
         private void Przycisk_Click(object sender, EventArgs e)
         {
-            Button klikniety = (Button)sender;
+            Button btn = (Button)sender;
+            string coTo = btn.Tag.ToString();
 
-            // Tu będziemy sprawdzać co jest pod spodem (Dydelf, Szop, Krokodyl) 
-            klikniety.Enabled = false; // Przycisk może być naciśnięty tylko raz 
-            klikniety.BackColor = Color.White; // Przykładowe odkrycie pola
+       
+            switch (coTo)
+            {
+                case "Dydelf":
+                    btn.Text = "🦡"; 
+                    btn.BackColor = Color.LightGreen;
+                    btn.Enabled = false;
+                    znalezioneDydelfy++;
+                    if (znalezioneDydelfy == UstawieniaGry.LiczbaDydelfow)
+                    {
+                        timerGry.Stop();
+                        MessageBox.Show("Sukces! Wszystkie Dydelfy odnalezione."); 
+                    }
+                    break;
+
+                case "Szop":
+                    btn.Text = "🦝";
+                    btn.BackColor = Color.Orange;
+                    btn.Enabled = false;
+                    ostatniSzop = btn;
+                    timerSzop.Interval = 2000;
+                    timerSzop.Start();
+                    break;
+
+                case "Krokodyl":
+                    if (!krokodylAktywny)
+                    {
+                        btn.Text = "🐊";
+                        btn.BackColor = Color.Red;
+                        krokodylAktywny = true;
+                        timerKrokodyl.Start(); // Startujemy 2 sekundy na reakcję 
+                    }
+                    else
+                    {
+                        // Gracz zdążył kliknąć drugi raz!
+                        timerKrokodyl.Stop();
+                        krokodylAktywny = false;
+                        btn.Text = "";
+                        btn.BackColor = Color.Gray;
+                        btn.Enabled = true; // Pozwalamy kliknąć ponownie później
+                    }
+                    break;
+
+                default:
+                    btn.BackColor = Color.White; // Puste pole 
+                    break;
+            }
         }
         private void FormGame_Load(object sender, EventArgs e)
         {
@@ -66,6 +196,21 @@ namespace lab6
         private void lblCzas_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void timerGry_Tick(object sender, EventArgs e)
+        {
+            if (pozostalyCzas > 0)
+            {
+                pozostalyCzas--;
+                lblCzas.Text = pozostalyCzas + " s";
+            }
+            else
+            {
+                timerGry.Stop();
+                MessageBox.Show("Gra kończy się porażką gdy upłynie ustalony czas."); 
+        this.Close();
+            }
         }
     }
 }
