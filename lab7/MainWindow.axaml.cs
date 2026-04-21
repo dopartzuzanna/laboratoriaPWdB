f6c98f7ae71f810fc917731e55280e7f68a2cae5
zuzanna <134936492+dopartzuzanna@users.noreply.github.com>
2026-04-21 18:36:17 +0200

    lab7

--- lab7/MainWindow.axaml.cs
+++ lab7/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace lab7
 {
@@ -8,5 +11,27 @@ namespace lab7
         {
             InitializeComponent();
         }
+            public void ButtonClicked(object source, RoutedEventArgs args)
+        {
+            string dna = InputDNA.Text?.ToUpper();
+
+            if (string.IsNullOrEmpty(dna) || dna.Length < 4)
+            {
+                ResultText.Text = "Sekwencja jest za krótka!";
+                return;
+            }
+
+            var counts = new Dictionary<string, int>();
+
+            for (int i = 0; i <= dna.Length - 4; i++)
+            {
+                string kmer = dna.Substring(i, 4);
+                if (counts.ContainsKey(kmer)) counts[kmer]++;
+                else counts[kmer] = 1;
+            }
+
+            ResultText.Text = string.Join("\n", counts.Select(x => $"{x.Key}: {x.Value}"));
+        }
+    
     }
 }
